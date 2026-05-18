293da5b45a9f928fa04ce4e78219cd4270e27203
The-Ayush001 <ayush007arm@.com>
2026-05-18 15:35:29 +0530

    insert node  using recursion

--- src/L44_Linked_List/Main.java
+++ src/L44_Linked_List/Main.java
@@ -5,33 +5,35 @@ import java.awt.datatransfer.FlavorListener;
 public class Main {
     static void main() {
 //        // Add the node at first position. --> list1.insetFirst() method call
-//        Single_LinkedList list1 = new Single_LinkedList();
-//        list1.insertFirst(1);
-//        list1.insertFirst(2);
-//        list1.insertFirst(3);
-//        list1.insertFirst(4);
+        Single_LinkedList list1 = new Single_LinkedList();
+        list1.insertFirst(1);
+        list1.insertFirst(2);
+        list1.insertFirst(3);
+        list1.insertFirst(4);
 
-//        list1.display();
+        list1.insertRec(20,3);
+
+        list1.display();
 
 //        // Add the node at last position. --> list1.insetLast() method call
-//        Single_LinkedList list2 = new Single_LinkedList();
-//        list2.insetLast(1);
-//        list2.insetLast(2);
-//        list2.insetLast(3);
-//        list2.insetLast(4);
-//
-//        list2.display();
-//
-//        list2.insert(2,5);
-//
-//        list2.insert(0,9);
-//        list2.insetLast(6);
-//        list2.insert(3,8);
-//        list2.insert(5,7);
-//        list2.insetLast(3);
-//        list2.insert(3,0);
-//
-//        list2.display();
+        Single_LinkedList list2 = new Single_LinkedList();
+        list2.insetLast(1);
+        list2.insetLast(2);
+        list2.insetLast(3);
+        list2.insetLast(4);
+
+        list2.display();
+
+        list2.insert(2,5);
+
+        list2.insert(0,9);
+        list2.insetLast(6);
+        list2.insert(3,8);
+        list2.insert(5,7);
+        list2.insetLast(3);
+        list2.insert(3,0);
+
+        list2.display();
 
 //        Doubly_Linked_List list = new Doubly_Linked_List();
 //        list.insertFirst(1);
@@ -46,16 +48,16 @@ public class Main {
 //
 //        list.insert(10 , 1);
 
-        CLL list = new CLL();
-        list.insert(10);
-        list.insert(20);
-        list.insert(40);
-        list.insert(50);
-        list.insert(60);
-        list.insert(70);
-
-        list.display();
-        list.delete(50);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(10);
+//        list.insert(20);
+//        list.insert(40);
+//        list.insert(50);
+//        list.insert(60);
+//        list.insert(70);
+//
+//        list.display();
+//        list.delete(50);
+//        list.display();
     }
 }
--- src/L44_Linked_List/Single_LinkedList.java
+++ src/L44_Linked_List/Single_LinkedList.java
@@ -71,8 +71,22 @@ public class Single_LinkedList {
         Node newNode = new Node(val, temp.next);
 //        newNode.next = temp.next;
         temp.next = newNode;
-
         size++;
+    }
+
+    //insert using recursion
+    public void insertRec(int val, int index){
+        head = insertRec(val, index, head);
+
+    }
+    private Node insertRec(int val, int index, Node node){
+        if(index == 0){
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+        node.next = insertRec(val, index-1, node.next);
+        return node;
     }
     // Display the list
     public void display(){
