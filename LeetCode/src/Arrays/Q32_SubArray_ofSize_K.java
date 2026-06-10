988b4813689d6a44e398fb9baba077d7d1fd40e7
The-Ayush001 <ayush007arm@gmail.com>
2026-06-10 09:25:21 +0530

    Sub Array

---
+++ LeetCode/src/Arrays/Q32_SubArray_ofSize_K.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.*;
+
+public class Q32_SubArray_ofSize_K {
+    public static List<List<Integer>> list = new ArrayList<>();
+
+    static void main() {
+        int[] nums = {1,3,2};
+        int k = 2;
+
+        System.out.print("Finding the length of subArray: ");
+        System.out.println(maxTotalValue(nums, k)); // call maxTotalValue method
+        System.out.println(list);
+    }
+
+    public static long maxTotalValue(int[] nums, int k) {
+
+        findSubArray(nums, k, new ArrayList(), 0);
+
+        return list.size();
+    }
+    public static void findSubArray(int[] arr, int k, ArrayList<Integer> subList, int ind){
+        // Base condition
+        if(subList.size() == k){
+            list.add(new ArrayList(subList));
+            return ;
+        }
+
+        for(int i = ind; i < arr.length; i++){
+            // insert the element of the array
+            subList.add(arr[i]);
+
+            findSubArray(arr, k, subList, i+1);
+
+            // backtrack
+            subList.remove(subList.size()-1);
+        }
+
+    }
+
+
+}
