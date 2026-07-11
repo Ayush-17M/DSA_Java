11dd1893e4d745fefbfcaec8cd5a52561b46496e
Ayush-17M <ayush007arm@gmail.com>
2026-07-11 18:51:19 +0530

    Tree

--- LeetCode/src/Tree/Q27_Count_Tree_Node_222.java
+++ LeetCode/src/Tree/Q27_Count_Tree_Node_222.java
@@ -1,4 +1,34 @@
 package Tree;
 
 public class Q27_Count_Tree_Node_222 {
+
+    // Optimize:
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+// Not Optimize:
+    // public int countNodes(TreeNode root) {
+    //     int count = 0;
+
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
+
+    //     while(!queue.isEmpty()){
+    //         TreeNode node = queue.poll();
+    //         if(node == null){
+    //             continue;
+    //         }
+    //         count++;
+    //         queue.offer(node.left);
+    //         queue.offer(node.right);
+    //     }
+
+    //     return count;
+    // }
+
 }
--- LeetCode/src/Tree/Q28_BT_Paths.java
+++ LeetCode/src/Tree/Q28_BT_Paths.java
@@ -1,4 +1,33 @@
 package Tree;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q28_BT_Paths {
+
+    List<String> list = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+        helper(root, "");
+        return list;
+    }
+
+    public void helper(TreeNode node, String str) {
+
+        if (node != null && node.left == null && node.right == null) { // leaf node
+            str += node.val;
+            list.add(str);
+            return;
+        }
+        str += node.val + "->";
+
+        if (node.left != null) { // if left node are exist.
+            helper(node.left, str);
+        }
+        if (node.right != null) { // if right node are exist.
+            helper(node.right, str);
+        }
+
+        return;
+    }
 }
